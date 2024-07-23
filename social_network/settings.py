008a84053644b50725682a3f13bd29289bca0d35
iman <imanmozafari.neonser@gmail.com>
2024-07-23 18:05:38 +0330

    started apps home, accounts, post

--- social_network/settings.py
+++ social_network/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
+    'accounts.apps.AccountsConfig',
+    'post.apps.PostConfig',
+
 ]
 
 MIDDLEWARE = [
