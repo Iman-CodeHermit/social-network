1e1a6cf871ed7024d2009ab08bf7204f97801271
iman <imanmozafari.neonser@gmail.com>
2024-07-24 04:27:37 +0330

    create class UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserChangeForm, UserCreationForm
+from .models import User
+from django.contrib.auth.models import Group
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phone_number', 'username')
+    list_filter = ('is_admin',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'phone_number', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'bio', 'birthday')}),
+        ('permissions', {'fields': ('is_active', 'is_admin')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('email', 'phone_number', 'last_name', 'first_name', 'birthday', 'password1', 'password2')}),
+    )
+
+    readonly_fields = ('last_login',)
+    search_fields = ('username',)
+    ordering = ('last_login')
+    filter_horizontal = ()
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
