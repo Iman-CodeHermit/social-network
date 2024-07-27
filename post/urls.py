69c64264a57369260e8542c6f9c97f6a518cb87e
iman <imanmozafari.neonser@gmail.com>
2024-07-27 23:07:28 +0330

    post deletion process

--- post/urls.py
+++ post/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'post'
 urlpatterns = [
     path('post/<int:post_id>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('update/<int:post_id>/', views.PostUpdateView.as_view(), name='post_update'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from post.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 # Create your views here.
 class PostDetailView(View):
@@ -8,3 +10,18 @@ class PostDetailView(View):
         post = Post.objects.get(id = post_id, slug = post_slug)
         return render(request, 'post/detail.html', {'post': post})
         
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, 'post delete successfully!', 'success')
+            return redirect('home:home')
+        messages.error(request, 'you cant delete this post!', 'danger')
+        return render(request, 'post/detail.html')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    def get(self, request):
+        pass
