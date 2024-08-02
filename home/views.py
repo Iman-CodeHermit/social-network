a8200f2a480fe79ffe528238a9a09756f7374448
iman <imanmozafari.neonser@gmail.com>
2024-08-02 22:45:14 +0330

    add search for searching in posts in home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from post.models import Post
+from .forms import PostSearchForm
 # Create your views here.
 class HomeView(View):
+    form_class = PostSearchForm
     def get(self, request):
         posts = Post.objects.all()
-        return render(request, 'home/home.html', {'posts': posts})
+        if request.GET.get('search'):
+            posts = posts.filter(body__contains=request.GET['search'])
+        return render(request, 'home/home.html', {'posts': posts, 'form': self.form_class})
